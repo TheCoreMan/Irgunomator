8b753b3e4eb7b0ce23c7c0c29120b3734c1de3e4
TheCoreMan <dude500@gmail.com>
2017-06-24 20:53:52 +0300

    refactored main a bit

--- main.py
+++ main.py
@@ -4,36 +4,38 @@ import handlers
 from config import config
 
 
-# Connecting to Telegram API
-# Updater retrieves information and dispatcher connects commands
-updater = Updater(token=config['DEFAULT']['token'])
-dispatcher = updater.dispatcher
+def get_handlers():
+    start_handler = CommandHandler('start', handlers.start.start)
+    help_handler = CommandHandler('help', handlers.start.start)
+    support_handler = CommandHandler('support', handlers.support.support, pass_args=True)
+    list_requests = CommandHandler('list', handlers.list_requests.list_requests)
+    show_request = CommandHandler('show', handlers.unknown.unknown)
+    close_request = CommandHandler('close', handlers.unknown.unknown)
+    unknown_handler = MessageHandler([Filters.command], handlers.unknown.unknown)
+    # Message handler must be the last one
+    support_msg_handler = MessageHandler([Filters.text], handlers.support.support_message)
+
+    return [start_handler, help_handler, support_handler, list_requests, show_request, close_request,
+            unknown_handler, support_msg_handler]
 
 
-# creating handlers
-start_handler = CommandHandler('start', handlers.start.start)
-help_handler = CommandHandler('help', handlers.start.start)
-support_handler = CommandHandler('support', handlers.support.support, pass_args=True)
-list_requests = CommandHandler('list', handlers.list_requests.list_requests)
-show_request = CommandHandler('show', handlers.unknown.unknown)
-close_request = CommandHandler('close', handlers.unknown.unknown)
-support_msg_handler = MessageHandler([Filters.text], handlers.support.support_message)
-unknown_handler = MessageHandler([Filters.command], handlers.unknown.unknown)
+def get_updater():
+    # Connecting to Telegram API
+    # Updater retrieves information and dispatcher connects commands
+    updater = Updater(token=config['DEFAULT']['token'])
+    dispatcher = updater.dispatcher
 
-# adding handlers
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(support_handler)
-dispatcher.add_handler(list_requests)
-dispatcher.add_handler(show_request)
-dispatcher.add_handler(close_request)
-dispatcher.add_handler(help_handler)
-dispatcher.add_handler(unknown_handler)
+    bot_handlers = get_handlers()
 
-# Message handler must be the last one
-dispatcher.add_handler(support_msg_handler)
+    # populate dispatcher
+    for bot_handler in bot_handlers:
+        dispatcher.add_handler(bot_handler)
+
+    return updater
 
 
 def main():
+    updater = get_updater()
     updater.stop()
     updater.start_polling()
 
