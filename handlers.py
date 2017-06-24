a07e6bbee04e35487b2442a3622093c7198ab243
TheCoreMan <dude500@gmail.com>
2017-06-24 20:25:58 +0300

    Added infection to listings. LOL

--- handlers.py
+++ handlers.py
@@ -1,7 +1,8 @@
 import requests_dal
 import telegram
 import config
-
+import inflect
+inflection = inflect.engine()
 
 def start(bot, update):
     """
@@ -78,24 +79,29 @@ def unknown(bot, update):
     """
         Placeholder command when the user sends an unknown command.
     """
-    msg = "Sorry, I don't know what you're asking for."
+    msg = "Sorry, I don't know what you're asking for.\nUse /help to see what I can do!"
     bot.send_message(chat_id=update.message.chat_id,
                      text=msg)
 
 
 def show_requests(bot, update):
     all_open_requests = requests_dal.get_all_open_requests()
+
     if update.message.chat_id == config.support_chat_id:
         bot.send_message(
             chat_id=config.support_chat_id,
-            text="There are {0} open requests".format(len(all_open_requests)))
+            text="There are {0} open {1}.".format(
+                len(all_open_requests),
+                inflection.plural("request", len(all_open_requests))))
         for open_request in all_open_requests:
             bot.send_message(chat_id=config.support_chat_id, text=repr(open_request))
     else:
         my_open_requests = filter(lambda x: x._creator == update.message.chat.id, all_open_requests)
         bot.send_message(
             chat_id=update.message.chat_id,
-            text="There are {0} open requests".format(len(my_open_requests)))
+            text="You have {0} open {1}.".format(
+                len(my_open_requests),
+                inflection.plural("request", len(my_open_requests))))
         for open_request in my_open_requests:
             bot.send_message(chat_id=update.message.chat_id, text=repr(open_request))
 
