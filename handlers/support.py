1ccffb6add6bbf52c4ba4488f074c274e9ac666c
TheCoreMan <dude500@gmail.com>
2017-06-24 21:50:33 +0300

    Deleted unnecessary code

--- handlers/support.py
+++ handlers/support.py
@@ -23,24 +23,6 @@ def support_message(bot, update):
         the bot forwards the message to the support group.
     """
     if update.message.reply_to_message:
-        if update.message.reply_to_message.text == messages.ASK_FOR_NEW_REQUEST:
-            support_request = update.message.text
-            request_id = update.update_id
-
-            new_request = requests_dal.Request(update.message, request_id)
-            new_request.append_message(support_request)
-            new_request.save()
-
-            support_chat_message_text = "{0}\n{1}\nPlease reply to this request.".format(request_id, support_request)
-            bot.send_message(
-                chat_id=int(config.config['DEFAULT']['support_chat_id']),
-                text=support_chat_message_text)
-
-            user_message_text = "Received new request:\n{0}".format(support_request)
-            bot.send_message(chat_id=update.message.chat_id,
-                             text=user_message_text)
-            return
-
         req_id = update.message.reply_to_message.text.split('\n')[0]
         req = requests_dal.get_request(req_id)
         req.append_message(update.message.text)
